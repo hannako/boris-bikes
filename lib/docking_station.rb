64bc19544cd9ec2eb8f6f626fdac744a6c964a0c
Giancarlo Fabrizi <gfabrizi@Giancarlos-Air.lan>
2016-07-08 16:17:01 +0200

    exercise 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -12,14 +12,29 @@ end
 # end
 
   def release_bike
-    raise 'No bikes available' if @bikes.empty?
+    raise 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-      raise 'Docking station is full' if @bikes.length >= 20
+      raise 'Docking station is full' if full?
       @bikes << bike
       @bikes[0]
     end
+
+    private
+    def full?
+      if @bikes.length >= 20
+        true
+      end
+    end
+
+      def empty?
+        if @bikes.empty?
+          true
+        end
+
+      end
+
 
 end
